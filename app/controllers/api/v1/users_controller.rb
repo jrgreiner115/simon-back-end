cc32cf1630c286edd0fb564da200541704f8ab68
github username <jrgreiner115@gmail.com>
2018-06-28 10:56:14 -0400

    registration auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,27 +5,27 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(userparams)
+    @user = User.new(user_params)
     if @user.save
 
       payload = {
-        name: @user.name
-        username: @user.username
+        name: @user.name,
+        username: @user.username,
         id: @user.id
       }
 
       token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
 
       render json: {
-        token
+        token: token,
         username: @user.username,
         id: @user.id,
         recordings: @user.recordings
       }
     else
-      render json: {
-        errors: @user.errors.full_message} status: :unprocessable_entity
-      }
+        render json: {
+        errors: @user.errors.full_messages
+      }, status: :unprocessable_entity
     end
   end
 
@@ -36,6 +36,6 @@ class Api::V1::UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :password, :username)
+      params.permit(:name, :password, :username)
     end
 end
